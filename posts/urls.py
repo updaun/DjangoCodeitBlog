e4e5f4120b72c44f5a3cca6072fdb65ca9330840
updaun <updauney@daum.net>
2021-09-26 23:20:21 +0900

    urls setting

---
+++ posts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('', views.index),
+    # path('posts', views.post_list),
+    # path('posts/new', view.post_create),
+    # path('posts/<int:post_id>/', view.post_detail),
+    # path('posts/<int:post_id>/edit', view.post_update),
+    # path('posts/<int:post_id>/delete', view.post_delete),
+]   
