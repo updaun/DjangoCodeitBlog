fdcee7bbf4eb39f29068b3cdaa527aefcec8f5b4
updaun <updauney@daum.net>
2021-09-27 15:30:37 +0900

    add Post models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    # 글의 제목
+    title = models.CharField(max_length=50)
+    # 글의 내용
+    content = models.TextField()
+    # 작성일
+    dt_created = models.DateField(verbose_name="Date Created", auto_now_add=True)
+    # 수정일
+    dt_modified = models.DateField(verbose_name="Date Modified", auto_now=True)
+
+
+    def __str__(self) -> str:
+        return self.title
