6a3dfecdba32731ca0cd41fd67992bc19d7046a3
updaun <updauney@daum.net>
2021-10-22 17:15:16 +0900

    post detail

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # path('', views.index),
     path('posts', views.post_list),
     # path('posts/new', view.post_create),
-    # path('posts/<int:post_id>/', view.post_detail),
+    path('posts/<int:post_id>/', views.post_detail),
     # path('posts/<int:post_id>/edit', view.post_update),
     # path('posts/<int:post_id>/delete', view.post_delete),
 ]   
