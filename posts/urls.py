a2a2ed22a5c4f623f064e3345ba8bb2ec16eabfb
updaun <updauney@daum.net>
2021-10-25 09:12:30 +0900

    use url name

--- posts/urls.py
+++ posts/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     # path('', views.index),
-    path('posts/', views.post_list),
+    path('posts/', views.post_list, name='post-list'),
     # path('posts/new', view.post_create),
-    path('posts/<int:post_id>/', views.post_detail),
+    path('posts/<int:post_id>/', views.post_detail, name='post-detail'),
     # path('posts/<int:post_id>/edit', view.post_update),
     # path('posts/<int:post_id>/delete', view.post_delete),
 ]   
